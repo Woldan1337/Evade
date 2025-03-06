c02d649de7aa159370babadd97bc131c427f4ead
Woldan1337 <woldancontact@gmail.com>
2025-03-06 20:55:59 +0300

    EvadeMain.lua

--- EvadeMain.lua
+++ EvadeMain.lua
@@ -11,6 +11,7 @@ Esp.Tracers = false
 Esp.Players = false
 Esp.Distance = false
 Esp.highlight = false
+DPLib.DPlayerESP = false
 
 Esp.Settings.BoxColor = Color3.fromRGB(0, 255, 0)
 Esp.Settings.TracerColor = Color3.fromRGB(255, 0, 0)
